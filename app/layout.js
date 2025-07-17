1b07bc6b796fe2b3676db21e58f3c4b503e9c9e1
siva09072004 <sskshiva89@gmail.com>
2025-07-17 14:27:03 +0530

    first_project

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sessionwrapper from "../components/Sessionwrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +20,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+
+      <Sessionwrapper>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+      </Sessionwrapper>
     </html>
   );
 }
